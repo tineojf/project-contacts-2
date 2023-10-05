3ce89560c9f1286608117d69df8d7a7c9170eb22
tineojf <92743762+tineodev@users.noreply.github.com>
2023-10-05 11:40:54 -0500

    Add main menu

--- src/main/java/org/tineo/App.java
+++ src/main/java/org/tineo/App.java
@@ -1,13 +1,24 @@
 package org.tineo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("----------------------------------------");
+        System.out.println("Project Contacts");
+        System.out.println("Language: Java");
+        System.out.println("----------------------------------------");
+
+        System.out.println("1. Create a new contact");
+        System.out.println("2. List all contacts");
+        System.out.println("3. Exit");
+
+        System.out.println("----------------------------------------");
+        System.out.println("Enter an option: ");
+        int option = scanner.nextInt();
+//        System.out.println("Option: " + option);
+        System.out.println("----------------------------------------");
     }
 }
