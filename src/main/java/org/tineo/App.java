d8e49960c3a57105a8e572792dfe920b140a3a00
tineojf <92743762+tineodev@users.noreply.github.com>
2023-10-05 11:29:57 -0500

    Initial commit

---
+++ src/main/java/org/tineo/App.java
@@ -0,0 +1,13 @@
+package org.tineo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
